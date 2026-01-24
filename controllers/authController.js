c16b5c070941d7a646e01f5a717f180ae72608c4
nnennia <106754370+Nnennia@users.noreply.github.com>
2026-01-24 10:40:50 +0100

    minor fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,6 @@
 const jwt = require("jsonwebtoken");
-const nodemailer = require("nodemailer");
+
 const { v4: uuidv4 } = require("uuid");
-const fs = require("fs").promises;
-const path = require("path");
 
 const User = require("../models/User");
 const Customer = require("../models/Customer");
@@ -10,7 +8,7 @@ const Vendor = require("../models/Vendor");
 const Rider = require("../models/Rider");
 const OtpVerification = require("../models/OtpVerification");
 const RefreshToken = require("../models/RefreshToken");
-
+const { sendWelcomeEmail, sendOtpEmail } = require("../utilis/email.utils");
 const {
 	generateAccessToken,
 	generateRefreshToken,
@@ -18,71 +16,7 @@ const {
 const { requestSmsOtp, verifySmsOtp } = require("../utilis/kudiSmsHelper");
 const { getCoordsFromAddress } = require("../utilis/delivery");
 
-const transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		user: process.env.EMAIL_USER,
-		pass: process.env.EMAIL_PASS,
-	},
-});
-
 const generateOtp = () => Math.floor(1000 + Math.random() * 9000).toString();
-
-const sendWelcomeEmail = async (email, name) => {
-	try {
-		const templatePath = path.join(
-			__dirname,
-			"../templates/welcome-email.html",
-		);
-		let htmlTemplate = await fs.readFile(templatePath, "utf-8");
-
-		htmlTemplate = htmlTemplate.replace("{{name}}", name);
-
-		await transporter.sendMail({
-			from: process.env.EMAIL_USER,
-			to: email,
-			subject: "Welcome to OunjeFood - Eat Fresh, Spend Less, Order Fast!",
-			html: htmlTemplate,
-		});
-
-		//console.log(`Welcome email sent to ${email}`);
-		return true;
-	} catch (error) {
-		console.error("Error sending welcome email:", error);
-		return false;
-	}
-};
-
-const sendOtpEmail = async (email, otp, purpose = "verification") => {
-	try {
-		const templatePath = path.join(__dirname, "../templates/otp.html");
-		let htmlTemplate = await fs.readFile(templatePath, "utf-8");
-
-		const otpDigits = otp.split("");
-		htmlTemplate = htmlTemplate.replace("{{otp_1}}", otpDigits[0] || "0");
-		htmlTemplate = htmlTemplate.replace("{{otp_2}}", otpDigits[1] || "0");
-		htmlTemplate = htmlTemplate.replace("{{otp_3}}", otpDigits[2] || "0");
-		htmlTemplate = htmlTemplate.replace("{{otp_4}}", otpDigits[3] || "0");
-
-		const subject =
-			purpose === "login"
-				? "Your OunjeFood Login Code"
-				: "Verify Your OunjeFood Email";
-
-		await transporter.sendMail({
-			from: process.env.EMAIL_USER,
-			to: email,
-			subject: subject,
-			html: htmlTemplate,
-		});
-
-		//console.log(`OTP email sent to ${email}`);
-		return true;
-	} catch (error) {
-		console.error("Error sending OTP email:", error);
-		throw error;
-	}
-};
 
 const register = async (req, res) => {
 	try {
