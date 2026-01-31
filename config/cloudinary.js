0a42fd87157b6d55535ea29bfc137bf77e22bfd9
Oluwatise Ajayi <dr393462@gmail.com>
2026-01-31 14:07:49 +0100

    fix config

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,6 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
-const CloudinaryStorage = require("multer-storage-cloudinary");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
