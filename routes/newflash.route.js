8c179b563956667955beb2fa124cf39ae39ca6ae
nnennia <106754370+Nnennia@users.noreply.github.com>
2026-02-03 17:41:45 +0100

    commit

--- routes/newflash.route.js
+++ routes/newflash.route.js
@@ -4,13 +4,13 @@ const newsController = require("../controllers/newsflashController");
 
 const router = express.Router();
 
-router.post("/create", newsBannerUpload.single("image"), newsController.create);
+//router.post("/create", newsBannerUpload.single("image"), newsController.create);
 router.get("/get", newsController.getAll);
 router.get("/:id", newsController.getOne);
-router.put(
-	"/update/:id",
-	newsBannerUpload.single("image"),
-	newsController.update,
-);
-router.delete("/delete/:id", newsController.remove);
+// router.put(
+// 	"/update/:id",
+// 	newsBannerUpload.single("image"),
+// 	newsController.update,
+// );
+// router.delete("/delete/:id", newsController.remove);
 module.exports = router;
