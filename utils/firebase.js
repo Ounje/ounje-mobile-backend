36b203636c5ad52b0888ab0d4e1f0c7f49655c21
nnennia <106754370+Nnennia@users.noreply.github.com>
2026-04-18 19:27:55 +0100

    minor fixes

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,4 +1,5 @@
 const admin = require("firebase-admin");
+const fs = require("fs");
 const logger = require("./logger");
 
 function initFirebase() {
@@ -8,11 +9,12 @@ function initFirebase() {
 		if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
 			serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 			if (serviceAccount.private_key) {
-				serviceAccount.private_key = serviceAccount.private_key.replace(
-					/\\n/g,
-					"\n",
-				);
+				serviceAccount.private_key = serviceAccount.private_key
+					.replace(/\\\\n/g, "\n")
+					.replace(/\\n/g, "\n");
 			}
+		} else if (fs.existsSync("/etc/secrets/.serviceAccountKey.json")) {
+			serviceAccount = require("/etc/secrets/.serviceAccountKey.json");
 		} else {
 			serviceAccount = require("../config/serviceAccountKey.json");
 		}
