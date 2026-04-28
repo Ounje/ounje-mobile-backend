1101bc32f3719ebab7d5edb6b962b6efc604152c
nnennia <106754370+Nnennia@users.noreply.github.com>
2026-04-28 08:52:15 +0100

    minor fixes

--- models/Promotion.js
+++ models/Promotion.js
@@ -2,24 +2,29 @@ const mongoose = require("mongoose");
 const toJSON = require("./plugins/toJSON.plugin");
 
 const promotionSchema = new mongoose.Schema(
-    {
-        code: { type: String, required: true, unique: true, uppercase: true },
-        description: String,
-        type: {
-            type: String,
-            enum: ["percentage", "fixed_amount"],
-            required: true,
-        },
-        value: { type: Number, required: true }, // Percentage (0-100) or Amount
-        maxDiscount: Number, // Cap for percentage
-        minOrderValue: { type: Number, default: 0 },
-        usageLimit: Number,
-        usedCount: { type: Number, default: 0 },
-        startsAt: Date,
-        expiresAt: Date,
-        isActive: { type: Boolean, default: true },
-    },
-    { timestamps: true },
+	{
+		code: { type: String, required: true, unique: true, uppercase: true },
+		description: String,
+		type: {
+			type: String,
+			enum: ["percentage", "fixed_amount"],
+			required: true,
+		},
+		value: { type: Number, required: true },
+		maxDiscount: Number,
+		minOrderValue: { type: Number, default: 0 },
+		usageLimit: Number,
+		usedCount: { type: Number, default: 0 },
+		usedBy: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "User",
+			default: [],
+		},
+		startsAt: Date,
+		expiresAt: Date,
+		isActive: { type: Boolean, default: true },
+	},
+	{ timestamps: true },
 );
 
 promotionSchema.plugin(toJSON);
