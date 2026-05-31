4c54c06d281aea8ba8376ccca56b31711ba3358c
Alphacreato <dandykelvin98@gmail.com>
2026-06-01 00:16:28 +0100

    Minor fix

--- controllers/foodItemController.js
+++ controllers/foodItemController.js
@@ -1118,6 +1118,7 @@ const getVendorsByCategory = async (req, res) => {
 							"active",
 						],
 					},
+					storeDetails: "$vendor.storeDetails",
 					averageRating: { $ifNull: ["$vendor.averageRating", 0] },
 					totalRating: { $ifNull: ["$vendor.ratingCount", 0] },
 					deliveryFee: {
